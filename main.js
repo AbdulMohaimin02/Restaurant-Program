ed9a1c93d5065104482277dafda1fb473bbb738d
Abdul Mohaimin <abdul.mohaimin@thebeansgroup.com>
2022-03-11 16:22:22 +0000

    Final commit ready for upload to github

--- main.js
+++ main.js
@@ -2,27 +2,5 @@ const {Restaurant} = require('./classes/restaurant')
 const {Menu} = require('./classes/menu')
 const {Item} = require('./classes/item')
 
-const util = require('util')
-
-Item.init()
-Menu.init()
-Restaurant.init()
-
-
-
-// const Nobu = new Restaurant('Nobu','https://image.url')
-// const landFood = new Menu(Nobu.id,'Land Food')
-// Nobu.addMenu(landFood)
-
-
-// const blackCod = new Item(landFood.id,'Black Cod', '£40.00')
-// landFood.addItem(blackCod)
-
-
-// console.log(Menu.all)
-console.log(util.inspect(Restaurant.all, {depth: Infinity,colors: true}))
-// console.log(Restaurant.all)
-// console.log(Item.all)
-
-
+module.exports = {Restaurant,Menu,Item}
 
